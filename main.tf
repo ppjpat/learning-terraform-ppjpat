91b4969045e11a85f51d209d06950a6ec9a915a3
ppjpat <138362039+ppjpat@users.noreply.github.com>
2023-12-08 16:06:42 +0100

    autoscaling commit

--- main.tf
+++ main.tf
@@ -34,20 +34,23 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "7.3.1"
+
+  name     = "blog"
+  min_size = 1
+  max_size = 2
+  
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arms   = module.blog_alb.target_group_arms
+  security_groups     = [module.blog_sg.security_group_id]
+  
+  image_id      = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-
-  subnet_id = module.blog_vpc.public_subnets[0]
-
-  tags = {
-    Name = "Learning Terraform"
-  }
 }
 
-module "alb" {
+module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
 
   name            = "blog-alb"
