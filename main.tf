6ce8562b567c1bc9b4bf106bfae83f0f6c87dcb6
ppjpat <138362039+ppjpat@users.noreply.github.com>
2023-12-08 15:31:18 +0100

    fixing target_id

--- main.tf
+++ main.tf
@@ -53,7 +53,7 @@ module "alb" {
   name    = "blog-alb"
   vpc_id  = module.blog_vpc.vpc_id
   subnets = module.blog_vpc.public_subnets
-  security_groups = module.blog_sg.security_group_id
+  security_groups = module.blog_sg.security_group_id[0]
   
 
   listeners = {
@@ -83,10 +83,7 @@ module "alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      targets = {
-        target_id = aws_instance.blog.id
-        port = 80
-      }
+      target_id        = aws_instance.blog.id
     }
   }
 
